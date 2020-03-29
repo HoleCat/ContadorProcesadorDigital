99c2e8e753786ebbfc794802813369d042a5f334
Jorge <Jorge@LAPTOP-JMUV0TCC>
2020-03-28 20:12:58 -0500

    el commit

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\CONTADOR\ProcesadorDigital\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\CONTADOR\ProcesadorDigital\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\CONTADOR\PRUEBA\ContadorProcesadorDigital\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\CONTADOR\PRUEBA\ContadorProcesadorDigital\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
